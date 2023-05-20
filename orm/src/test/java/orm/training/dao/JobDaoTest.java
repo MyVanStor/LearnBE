f9ae1200365972e6acfaadafc4de4a2bea7bba9e
Nguyen Van <Vanhoa30903@gmail.com>
2023-05-20 16:40:01 +0700

    Buổi 4

---
+++ orm/src/test/java/orm/training/dao/JobDaoTest.java
@@ -0,0 +1,29 @@
+package orm.training.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import orm.training.entity.Jobs;
+
+public class JobDaoTest {
+    static JobDaoImpl jobDao;
+    
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        jobDao = new JobDaoImpl();
+    }
+    
+    @Test
+    void testSave1() throws Exception {
+        Jobs job = new Jobs("J01", "Java Dev1", 1000, 2000);
+        assertEquals(true, jobDao.save(job));
+    }
+    @Test
+    void testSave2() throws Exception {
+        Jobs job = new Jobs("J02", "Java Dev2", 1200, 2200);
+        assertEquals(true, jobDao.save(job));
+    }
+
+}
